c81046b6a7d397ff4d4919ab95a438e7c19a6bcc
Timo Lins <me@timo.sh>
2020-04-19 15:56:01 +0200

    Prevent jumps when aspect ratio is known

--- src/components/asset.tsx
+++ src/components/asset.tsx
@@ -13,7 +13,12 @@ const Asset: React.FC<{ block: BlockType }> = ({ block }) => {
   }
 
   const format = value.format;
-  const { block_width, block_height, display_source } = format;
+  const {
+    block_width,
+    block_height,
+    display_source,
+    block_aspect_ratio
+  } = format;
 
   const isImage = type === "image";
 
@@ -37,7 +42,21 @@ const Asset: React.FC<{ block: BlockType }> = ({ block }) => {
 
   if (type === "image") {
     const caption = value.properties.caption?.[0][0];
-    return <img style={style} alt={caption} src={src} />;
+    if (block_aspect_ratio) {
+      return (
+        <div
+          style={{
+            ...style,
+            paddingBottom: `${block_aspect_ratio * 100}%`,
+            position: "relative"
+          }}
+        >
+          <img className="notion-image-inset" alt={caption} src={src} />
+        </div>
+      );
+    } else {
+      return <img style={style} alt={caption} src={src} />;
+    }
   }
 
   if (type === "video") {
