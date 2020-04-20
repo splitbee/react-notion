dc4af69a70ff5c31d0c67ad29fd0af52d8dc9f5c
Sam Wight <samuelwight@gmail.com>
2020-04-20 02:14:47 -0500

    Add proper licensing.

--- src/block.tsx
+++ src/block.tsx
@@ -1,3 +1,21 @@
+/**
+ * Chorale: A blazing fast Notion page renderer.
+ * Copyright (C) 2020 Sam Wight
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import * as React from "react";
 import { DecorationType, BlockType, ContentValueType } from "./types";
 import Asset from "./components/asset";
@@ -50,7 +68,7 @@ interface Block {
   level: number;
 }
 
-export const Block: React.FC<Block> = props => {
+export const Block: React.FC<Block> = (props) => {
   const { block, parentBlock, children } = props;
   const blockValue = block?.value;
   switch (blockValue.type) {
--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,3 +1,20 @@
+/**
+ * Chorale: A blazing fast Notion page renderer.
+ * Copyright (C) 2020 Sam Wight
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 import React from "react";
 import { BlockMapType } from "./types";
 import { Block } from "./block";
@@ -11,7 +28,7 @@ interface NotionRendererProps {
 export const NotionRenderer: React.FC<NotionRendererProps> = ({
   level = 0,
   currentId,
-  blockMap
+  blockMap,
 }) => {
   const id = currentId || Object.keys(blockMap)[0];
   const currentBlock = blockMap[id];
@@ -24,7 +41,7 @@ export const NotionRenderer: React.FC<NotionRendererProps> = ({
       block={currentBlock}
       parentBlock={parentBlock}
     >
-      {currentBlock?.value?.content?.map(contentId => (
+      {currentBlock?.value?.content?.map((contentId) => (
         <NotionRenderer
           key={contentId}
           currentId={contentId}
