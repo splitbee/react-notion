4143211c6080ae7524743c7da5e09005351ab0c6
Timo Lins <me@timo.sh>
2020-04-20 18:58:39 +0200

    Make `code.tsx` simpler

--- src/components/code.tsx
+++ src/components/code.tsx
@@ -1,22 +1,20 @@
 import * as React from "react";
-import Prism from "prismjs";
+import { highlight, languages } from "prismjs";
 import "prismjs/components/prism-jsx";
 
 const Code: React.FC<{ code: string; language: string }> = ({
   code,
   language = "javascript"
 }) => {
+  const prismLanguage =
+    languages[language.toLowerCase()] || languages.javascript;
+
   return (
     <pre>
       <code
         className="notion-code"
         dangerouslySetInnerHTML={{
-          __html: Prism.highlight(
-            code,
-            Prism.languages[language.toLowerCase()] ||
-              Prism.languages.javascript,
-            language
-          )
+          __html: highlight(code, prismLanguage, language)
         }}
       />
     </pre>
