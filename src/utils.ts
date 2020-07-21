e971f84f923d9f1460f176130254a32e2e7c98db
Travis Fischer <fisch0920@gmail.com>
2020-07-21 10:04:31 -0400

    bugfix for block in page 99db4a0cad2d4025880ecbc1f08a6abf

--- src/utils.ts
+++ src/utils.ts
@@ -15,7 +15,7 @@ const groupBlockContent = (blockMap: BlockMapType): string[][] => {
 
   Object.keys(blockMap).forEach(id => {
     blockMap[id].value.content?.forEach(blockId => {
-      const blockType = blockMap[blockId]?.value.type;
+      const blockType = blockMap[blockId]?.value?.type;
 
       if (blockType && blockType !== lastType) {
         index++;
